be5310233853dba5b33f5ff8e929a5954d989f2e
Seba Mamby <s.mamby@gmail.com>
2025-11-11 14:01:25 -0300

    pdf con puppeter y css mejorado

--- components/impPDF/network.js
+++ components/impPDF/network.js
@@ -5,13 +5,13 @@ const path = require('path');
 const htmlPdf  = require('html-pdf')
 //const PDFDocument = require('pdfkit');
 //const jsPDF = require('jspdf');
-//const puppeteer = require('puppeteer')
+const puppeteer = require('puppeteer')
 
 
 const pathPDFs = 'pdfs' //path donde se guarda los PDF
 
 
-router.post('/', (req,res)=>{
+router.post('/', async (req,res)=> {
   //console.log(`[[router.post]] ${JSON.stringify(req.body)}`);
   const { html, filename } = req.body;
 
@@ -26,7 +26,7 @@ router.post('/', (req,res)=>{
   const filePath = path.join(__dirname, pathPDFs, filename)
   const filePathProduccion = path.join('../../pruebaPDF/',filename)
   //const filePathProduccion = path.join('../../Users/User/Documents/INMOBILIARIA/RECIBOS_Y_LIQUIDACIONES/mes_actual/',filename);
-  const cssPath = path.join(__dirname, '../../public/styles', 'impPDFincrustado.css');
+  const cssPath = path.join(__dirname, '../../public/styles', 'impPDF.css'); //'impPDFincrustado.css');
   console.log(`[[filepath]] ${filePathProduccion}`)
 
   let cssContent;
@@ -50,6 +50,7 @@ router.post('/', (req,res)=>{
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,500;0,700;1,200;1,600&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" media="all" href="./styles/impPDF.css">
     <style>${cssContent}</style>
   </head>
   <body style="width: 628px">
@@ -61,21 +62,23 @@ router.post('/', (req,res)=>{
   //console.log(`[[[network fichaPDFback]]]   ${fichaPDFBACK}`)
 
   //let file = { url: "http://127.0.0.1:5500/public/popimp.html" };
-  htmlPdf.create(fichaPDFBACK, options).toFile(filePathProduccion, (err, result) => { //fichaPDFBACK => file
-    if (err) {
-      console.error('Error generating PDF:', err);
-      return res.status(500).send('Error generating PDF');
-    }
-    res.status(200).send({ message: 'PDF generado y guardado correctamente', filePathProduccion: result.filename });
-  })
+  // htmlPdf.create(fichaPDFBACK, options).toFile(filePathProduccion, (err, result) => { //fichaPDFBACK => file
+  //   if (err) {
+  //     console.error('Error generating PDF:', err);
+  //     return res.status(500).send('Error generating PDF');
+  //   }
+  //   res.status(200).send({ message: 'PDF generado y guardado correctamente', filePathProduccion: result.filename });
+  // })
 
-  fs.writeFileSync('./public/prueba.html', fichaPDFBACK, 'utf8', (err) => {
+  let numRandom = Math.random() * 10;
+  fs.writeFileSync('./public/prueba'+numRandom+'.html', fichaPDFBACK, 'utf8', (err) => {
     if (err) {
         console.error('Error al guardar el archivo:', err);
         return res.status(500).send('Error al guardar el archivo');;
     }
     console.log('Archivo guardado exitosamente en la carpeta public como prueba.html');
     res.status(200).send({ message: 'Archivo guardado exitosamente' });
+    window.open('http://127.0.0.1:5500/prueba'+numRandom+'.html');
   });
 
   /////////////////////////////////////////////////////////////////
@@ -90,17 +93,29 @@ router.post('/', (req,res)=>{
   // });
 
   ///////////////////////////////////////////////////////////////
-  // async function convPDF(){
-  //   const browser = await puppeteer.launch();
-  //   const page = await browser.newPage();
-  //   await page.setContent(fichaPDFBACK, {waitUntil: 'domcontentloaded'})
-  //   const pdfBuffer = await page.pdf({
-  //     format: 'A4',
-  //     path: filePath,
-  //     printBackground: true
-  //   })
-  // }
-  // convPDF();
+    const SERVER_BASE_URL = 'http://127.0.0.1:5500';
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.setContent(fichaPDFBACK, {
+      waitUntil: 'networkidle0',
+      baseURL: SERVER_BASE_URL
+  })
+    await page.pdf({
+      format: 'A4',
+      path: filePath,
+      printBackground: true,
+      margin: {
+        top: '0',
+        bottom: '0',
+        left: '0',
+        right: '0',
+      },
+      scale: 0.9,
+    })
+    await browser.close();
+    res.status(200).send({ message: 'PDF generado y guardado correctamente', filePathProduccion });
+
+
 
 
 /////////////////////////////////////////
